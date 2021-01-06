b526ecec9103de175e5a8125f3b2975370331c93
Tatsuki Kitaoka <ksjfisa_sand@yahoo.co.jp>
2021-01-06 20:38:36 +0900

    Update Task_2 on Jan 6th

--- js/main.js
+++ js/main.js
@@ -1,63 +1,90 @@
 'use strict';
 {
+  const RESULT = document.getElementById('result'); // 出力結果の表示位置取得
+
   // 初期の【出力】を記憶
-  let defaultHTML = result.innerHTML;
+  const DEFAULT_HTML = RESULT.innerHTML;
   // // 初期のHTML記述を記憶
-  // let defaultHTML = document.body.innerHTML;
+  // const DEFAULT_HTML = document.body.innerHTML;
   
   // FizzBuzz処理
   document.querySelector('form').addEventListener('submit' , e => {
     e.preventDefault();
+    
+    // 【出力】を初期状態に戻す
+    RESULT.innerHTML = DEFAULT_HTML;
+    // // HTML記述を初期状態に戻す
+    // document.body.innerHTML = DEFAULT_HTML;
+    
+    let frag = document.createDocumentFragment(); // 文書の断片(組み立てたノードを保管するtemp容器)を作成
 
     const FIZZ = parseFloat(document.getElementById('FizzNum').value, 10); // 文字列から10進数に変換
     const BUZZ = parseFloat(document.getElementById('BuzzNum').value, 10); // 文字列から10進数に変換
 
-    let SumFizzNum = FIZZ;
-    let SumBuzzNum = BUZZ;
-    // console.log(typeof(SumFizzNum)); // ForDebug データ型確認
-    // console.log(Number.isInteger(SumFizzNum)); // ForDebug 整数判定
+    // let sumFizzNum = FIZZ;
+    // let sumBuzzNum = BUZZ;
+    // // console.log(typeof(sumFizzNum)); // ForDebug データ型確認
+    // // console.log(Number.isInteger(sumFizzNum)); // ForDebug 整数判定
 
-    let result = document.getElementById('result'); // 出力結果の表示位置取得
-    let frag = document.createDocumentFragment(); // 文書の断片(組み立てたノードを保管するtemp容器)を作成
-
-    if (Number.isInteger(SumFizzNum) && Number.isInteger(SumBuzzNum)
-      && (SumFizzNum != 0) && (SumBuzzNum != 0))
-    {
-      while (SumFizzNum < 100 || SumBuzzNum < 100) {
-        if (SumFizzNum < SumBuzzNum) {
-          // console.log('Fizz ' + SumFizzNum); // ForDebug
-          let p = document.createElement('p'); // 要素ノードpを作成
-          let text = document.createTextNode('Fizz ' + SumFizzNum); // テキストノードを作成
-          p.appendChild(text); // ノード同士の組み立て
-          frag.appendChild(p); // 要素ノードpをFragmentに追加
-          SumFizzNum = SumFizzNum + FIZZ; // 加算
-        } else if (SumBuzzNum < SumFizzNum) {
-          // console.log('Buzz ' + SumBuzzNum); // ForDebug
-          let p = document.createElement('p'); // 要素ノードpを作成
-          let text = document.createTextNode('Buzz ' + SumBuzzNum); // テキストノードを作成
-          p.appendChild(text); // ノード同士の組み立て
-          frag.appendChild(p); // 要素ノードpをFragmentに追加
-          SumBuzzNum = SumBuzzNum + BUZZ;
-        } else { // SumFizzNum === SumBuzzNum
-          // console.log('FizzBuzz ' + SumFizzNum); // ForDebug
-          let p = document.createElement('p'); // 要素ノードpを作成
-          let text = document.createTextNode('FizzBuzz ' + SumFizzNum); // テキストノードを作成
-          p.appendChild(text); // ノード同士の組み立て
-          frag.appendChild(p); // 要素ノードpをFragmentに追加
-          SumFizzNum = SumFizzNum + FIZZ;
-          SumBuzzNum = SumBuzzNum + BUZZ;
+    // if (Number.isInteger(sumFizzNum) && Number.isInteger(sumBuzzNum)
+    //   && (sumFizzNum != 0) && (sumBuzzNum != 0))
+    // {
+    //   while (sumFizzNum < 100 || sumBuzzNum < 100) {
+    //     if (sumFizzNum < sumBuzzNum) {
+    //       // console.log('Fizz ' + sumFizzNum); // ForDebug
+    //       let text = document.createTextNode(`Fizz ${sumFizzNum}`); // テキストノードを作成
+    //       const P = document.createElement('p'); // 要素ノードpを作成
+    //       P.appendChild(text); // ノード同士の組み立て
+    //       frag.appendChild(P); // 要素ノードpをFragmentに追加
+    //       sumFizzNum = sumFizzNum + FIZZ; // 加算
+    //     } else if (sumBuzzNum < sumFizzNum) {
+    //       // console.log('Buzz ' + sumBuzzNum); // ForDebug
+    //       let text = document.createTextNode(`Buzz ${sumBuzzNum}`); // テキストノードを作成
+    //       const P = document.createElement('p'); // 要素ノードpを作成
+    //       P.appendChild(text); // ノード同士の組み立て
+    //       frag.appendChild(P); // 要素ノードpをFragmentに追加
+    //       sumBuzzNum = sumBuzzNum + BUZZ;
+    //     } else { // sumFizzNum === sumBuzzNum
+    //       // console.log('FizzBuzz ' + sumFizzNum); // ForDebug
+    //       let text = document.createTextNode(`FizzBuzz ${sumFizzNum}`); // テキストノードを作成
+    //       const P = document.createElement('p'); // 要素ノードpを作成
+    //       P.appendChild(text); // ノード同士の組み立て
+    //       frag.appendChild(P); // 要素ノードpをFragmentに追加
+    //       sumFizzNum = sumFizzNum + FIZZ;
+    //       sumBuzzNum = sumBuzzNum + BUZZ;
+    //     }
+    //   }
+    // } else {
+    //   let text = document.createTextNode('整数値を入力してください'); // テキストノードを作成
+    //   frag.appendChild(text); // 要素ノードpをFragmentに追加
+    // }
+    
+    if (Number.isInteger(FIZZ) && Number.isInteger(BUZZ)
+    && (FIZZ != 0) && (BUZZ != 0)) {
+      for (let i=1; i<=99; i++){
+        if ( (i % FIZZ === 0) && (i % BUZZ === 0) ) {
+          let text = document.createTextNode(`FizzBuzz ${i}`); // テキストノードを作成
+          const P = document.createElement('p'); // 要素ノードpを作成
+          P.appendChild(text); // ノード同士の組み立て
+          frag.appendChild(P); // 要素ノードpをFragmentに追加
+        } else if (i % FIZZ === 0) {
+          let text = document.createTextNode(`Fizz ${i}`); // テキストノードを作成
+          const P = document.createElement('p'); // 要素ノードpを作成
+          P.appendChild(text); // ノード同士の組み立て
+          frag.appendChild(P); // 要素ノードpをFragmentに追加
+        } else if (i % BUZZ === 0) {
+          let text = document.createTextNode(`Buzz ${i}`); // テキストノードを作成
+          const P = document.createElement('p'); // 要素ノードpを作成
+          P.appendChild(text); // ノード同士の組み立て
+          frag.appendChild(P); // 要素ノードpをFragmentに追加
         }
       }
     } else {
       let text = document.createTextNode('整数値を入力してください'); // テキストノードを作成
       frag.appendChild(text); // 要素ノードpをFragmentに追加
     }
-    // 【出力】を初期状態に戻す
-    result.innerHTML = defaultHTML;
-    // // HTML記述を初期状態に戻す
-    // document.body.innerHTML = defaultHTML;
-    
+
     // Fragmentをまとめて文書ツリーに追加
-    result.appendChild(frag);
+    RESULT.appendChild(frag);
   });
 }
